b9f6507392dfdc25e168896c41901bf15e3d1443
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-23 20:22:09 +0530

    ADDED: PerfectNumber program

---
+++ src/com/bridgelabz/logicalprograms/PerfectNumber.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.logicalprograms;
+
+public class PerfectNumber {
+	public static void main(String[] args) {
+		int number = 8129;
+		int sum = 0;
+		for(int i=1; i<= number/2; i++) {
+			if(number%i == 0) {
+				sum+= i;
+			}
+		}
+		if(number == sum) {
+			System.out.println("This is Perfect number");
+		}
+		else {
+			System.out.println("This is not perfect number");
+		}
+	}
+}
