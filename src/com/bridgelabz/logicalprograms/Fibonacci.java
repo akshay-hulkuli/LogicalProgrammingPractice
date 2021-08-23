3ac7c0ad9d430683f43d7262912e903f807ac36b
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-23 19:59:04 +0530

    Initial upload

---
+++ src/com/bridgelabz/logicalprograms/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.logicalprograms;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		int fibonacciLimit = 15;
+		int previousTerm1 = 1;
+		int previousTerm2 = 0;
+		int currentTerm = 0;
+		System.out.print(previousTerm2+" "+previousTerm1+" ");
+		for(int i=2;i<= fibonacciLimit;i++) {
+			currentTerm = previousTerm1 + previousTerm2;
+			System.out.print(currentTerm+" ");
+			previousTerm2 = previousTerm1;
+			previousTerm1 = currentTerm;
+		}
+	}
+}
