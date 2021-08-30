bd24376248c0f03046a31326cc980608a525aa0f
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-30 16:04:55 +0530

    ADDED:gitignore to ignore unwanted files

--- src/com/bridgelabz/logicalprograms/PerfectNumber.java
+++ src/com/bridgelabz/logicalprograms/PerfectNumber.java
@@ -2,7 +2,7 @@ package com.bridgelabz.logicalprograms;
 
 public class PerfectNumber {
 	public static void main(String[] args) {
-		int number = 8129;
+		int number = 28;
 		int sum = 0;
 		for(int i=1; i<= number/2; i++) {
 			if(number%i == 0) {
